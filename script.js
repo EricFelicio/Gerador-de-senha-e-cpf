4038e737b58fa300200d9c1c8cc2e996604056e4
Eric <ericfaiscaextremo@gmail.com>
2022-04-05 22:48:55 -0300

    html e js basicos

---
+++ script.js
@@ -0,0 +1,12 @@
+let inputrange = document.querySelector("#inputrange");
+let button = document.querySelector("#button");
+
+let caracteres = document.querySelector("#caracteres");
+let senha = document.querySelector("#senha");
+
+let hide = document.querySelector("#hide");
+
+let charset = "1234567890!@#$%¨&*()_+qwertyuiopasdfghjklçzxcvbnm<>~;[]/";
+let novasenha = "";
+
+caracteres.innerHTML = inputrange.value;
